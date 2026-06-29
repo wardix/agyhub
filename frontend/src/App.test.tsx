f7a3ec7e18f21c24630e068c1fcf80cdeebf9eb2
Wardi <wardi@nusa.net.id>
2026-06-29 10:55:35 +0700

    fix frontend test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App'
 describe('App', () => {
   it('renders without crashing', () => {
     render(<App />)
-    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument()
+    expect(screen.getByText(/Get started/i)).toBeInTheDocument()
   })
 })
